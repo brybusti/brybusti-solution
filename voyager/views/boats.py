db0a6c699e2c1a9130d9da1175a4e32ca0737ec5
brybusti <60212097+brybusti@users.noreply.github.com>
2020-02-27 22:02:45 -0800

    All previously mentioned working, name imply boat working, and now working on rest.

--- voyager/views/boats.py
+++ voyager/views/boats.py
@@ -10,9 +10,18 @@ from voyager.db import get_db, execute
 def boats(conn):
     return execute(conn, "SELECT b.bid, b.name, b.color FROM Boats AS b")
 
+def boatSearch(conn, sName):
+    return execute(conn, "SELECT DISTINCT b.name FROM Boats AS b INNER JOIN Voyages AS v ON v.bid=b.bid INNER JOIN Sailors AS s ON s.sid=v.sid WHERE s.name="+sName)
+
 def views(bp):
     @bp.route("/boats")
     def _boats():
         with get_db() as conn:
             rows = boats(conn)
         return render_template("table.html", name="boats", rows=rows)
+    @bp.route("/boats/sailed-by")
+    def _boatSailed():
+        with get_db() as conn:
+            sName = '\'' + request.args.get("sailor-name") + '\''
+            rows = boatSearch(conn, sName)
+        return render_template("table.html", name="What boat did they sail?", rows=rows)
